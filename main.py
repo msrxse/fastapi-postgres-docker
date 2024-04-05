f651c40effff21f5fad87f776232c8c24db9ec17
msr <msrxse@gmail.com>
2024-04-05 11:14:23 +0100

    Add delete, update and put endpoints

--- main.py
+++ main.py
@@ -20,3 +20,36 @@ async def create_contact(
 @app.get("/api/contacts/", response_model=List[_schemas.Contact])
 async def get_contacts(db:_orm.Session = _fastapi.Depends(_services.get_db)):
   return await _services.get_all_contacts(db=db)
+
+@app.get("/api/contacts/{contact_id}/", response_model=_schemas.Contact)
+async def get_contact(contact_id, db:_orm.Session = _fastapi.Depends(_services.get_db)):
+  contact = await _services.get_contact(db=db, contact_id=contact_id)
+  if contact is None:
+    raise _fastapi.HTTPException(status_code=404, detail="Contact does not exist")
+  
+  return contact
+
+@app.delete("/api/contacts/{contact_id}/")
+async def delete_contact(contact_id:int, db:_orm.Session = _fastapi.Depends(_services.get_db)):
+  contact = await _services.get_contact(db=db, contact_id=contact_id)
+  if contact is None:
+    raise _fastapi.HTTPException(status_code=404, detail="Contact does not exist")
+  
+  await _services.delete_contact(contact, db=db)
+
+  return "successfully deleted the user"
+
+
+@app.put("/api/contacts/{contact_id}/", response_model=_schemas.Contact)
+async def update_contact(
+  contact_id:int, 
+  contact_data:_schemas.CreateContact,
+   db:_orm.Session = _fastapi.Depends(_services.get_db),
+):
+  contact = await _services.get_contact(db=db, contact_id=contact_id)
+  if contact is None:
+    raise _fastapi.HTTPException(status_code=404, detail="Contact does not exist")
+  
+  return await _services.update_contact(
+    contact_data=contact_data, contact=contact, db=db
+  )
--- services.py
+++ services.py
@@ -29,3 +29,25 @@ async def create_contact(
 async def get_all_contacts(db:"Session") -> List[_schemas.Contact]:
   contacts = db.query(_models.Contact).all()
   return list(map(_schemas.Contact.from_orm, contacts))
+
+async def get_contact(contact_id:int, db:"Session"):
+  contact = db.query(_models.Contact).filter(_models.Contact.id == contact_id).first()
+  return contact
+
+async def delete_contact(contact:_models.Contact, db:"Session"):
+  db.delete(contact)
+  db.commit()
+
+async def update_contact(
+  contact_data:_schemas.CreateContact, 
+  contact: _models.Contact, db:"Session",
+) -> _schemas.Contact:
+  contact.first_name = contact_data.first_name
+  contact.last_name = contact_data.last_name
+  contact.email = contact_data.email
+  contact.phone_number = contact_data.phone_number
+
+  db.commit()
+  db.refresh(contact)
+
+  return _schemas.Contact.from_orm(contact)
